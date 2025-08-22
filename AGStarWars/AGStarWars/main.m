88f79be4212b4343bb184e2741206fbbe0a3dd41
Ana Ganfornina <anies.llamani@gmail.com>
2025-08-22 19:36:09 +0200

    feat: Inicio de curso primer ejercicio

---
+++ AGStarWars/AGStarWars/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AGStarWars
+//
+//  Created by Ana on 22/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");// es el resultado de la linea de comandos. Si sale bien devuele un 0 y si sale mal devuelve algo que no es un 0
+    }
+    return 0;
+}
