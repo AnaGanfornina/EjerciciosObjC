9037dbb524ee82775aeb8d76490fbd123f1b7988
Ana Ganfornina Arques <ana@Mac-mini-de-Ana.local>
2025-08-25 00:10:31 +0200

    wip: Creación de clase.Desde macMini

---
+++ AGStarWars/AGStarWars/StarWartsCharacter.h
@@ -0,0 +1,16 @@
+//
+//  StarWartsCharacter.h
+//  AGStarWars
+//
+//  Created by Ana Ganfornina Arques on 23/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StarWartsCharacter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AGStarWars/AGStarWars/StarWartsCharacter.m
@@ -0,0 +1,12 @@
+//
+//  StarWartsCharacter.m
+//  AGStarWars
+//
+//  Created by Ana Ganfornina Arques on 23/8/25.
+//
+
+#import "StarWartsCharacter.h"
+
+@implementation StarWartsCharacter
+
+@end
